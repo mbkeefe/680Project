ad2a6f1f7b0b12c8cfacd1c47ee0e594f8c256c8
Mae Keefe <keefe_mae@bentley.edu>
2017-04-10 15:07:39 -0400

    Created tab layout with map on one tab and list of sitters on next

--- app/src/main/java/com/example/maebaldwin/petdaycare/BrowseSitters.java
+++ app/src/main/java/com/example/maebaldwin/petdaycare/BrowseSitters.java
@@ -3,28 +3,90 @@ package com.example.maebaldwin.petdaycare;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /**
  * Created by maebaldwin on 3/7/17.
  */
 
 // Update my comments
-    // New comments to commit
+    // New comments to commit 2
 
-public class BrowseSitters extends Activity {
 
+
+
+public class BrowseSitters extends FragmentActivity implements OnMapReadyCallback, AdapterView.OnItemClickListener {
+    private TabHost tabs;
+    private GoogleMap map;
+    private float zoom = 11.5f;
+    private ListView sitters;
+    private ArrayList<String> listArray = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse_sitters);
+
+
+        tabs = (TabHost)findViewById(R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec;
+
+        // Initialize a TabSpec for the maps tab
+        spec = tabs.newTabSpec("t1");
+        spec.setContent(R.id.Map);
+        spec.setIndicator("Map");
+        tabs.addTab(spec);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapfrag);
+        mapFragment.getMapAsync(this);
+
+        // Initialize a TabSpec for the list tab
+        spec = tabs.newTabSpec("t1");
+        spec.setContent(R.id.List);
+        spec.setIndicator("List");
+        tabs.addTab(spec);
+
+        // For Sitter list tab
+        sitters = (ListView)findViewById(R.id.listView);
+        sitters.setOnItemClickListener(this);
+        adapter = new ArrayAdapter<String>(this, R.layout.list, listArray);
+        sitters.setAdapter(adapter);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.submenu, menu);
         return true;
+    }
+
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+
+        LatLng center = new LatLng(42.3498,-71.2251);
+        map.moveCamera(CameraUpdateFactory.newLatLng(center));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(center, zoom));
+
     }
 
     /*
@@ -37,6 +99,12 @@ public class BrowseSitters extends Activity {
         return true;
     }
     */
+
+
+    // When a list item is clicked
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+    }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemID = item.getItemId();
